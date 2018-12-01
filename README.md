b34bb67240215cf0794da733023a46db4652225a
hungnt13 <hungnt13@topica.edu.vn>
2018-12-01 11:52:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Setup server ubuntu 16.04 for Machine learning deployment with PHP, docker, apache2, python3.6, MySQL
+## Install package
+```
+chmod +x setup.sh
+./setup.sh
+```
+## Config apache 
+### Change Apache's root directory, run:
+```
+sudo nano /etc/apache2/sites-available/000-default.conf
+```
+### Config apache2 for php
+```
+sudo nano /etc/apache2/mods-enabled/dir.conf
+```
+You will see:
+```
+<IfModule mod_dir.c>
+    DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm
+</IfModule>
+```
+Change to:
+```
+<IfModule mod_dir.c>
+    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+</IfModule>
+```
+### Change default root folder
+```
+sudo nano /etc/apache2/apache2.conf
+```
+Use **Ctrl+W** and search for Directory (It should be in line 153)
+
+It should look like this
+```
+<Directory />
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all denied
+</Directory>
+```
+Change to
+```
+<Directory />
+    Options Indexes FollowSymLinks Includes ExecCGI
+    AllowOverride All
+    Require all granted
+</Directory>
+```
+
+sudo service apache2 restart
+### Install MySQL
+```
+sudo apt install mysql-server -y
+sudo mysql_secure_installation
+```
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove 
+sudo apt autoclean
+# unzip
+sudo apt-get install unzip -y
+# python3.6
+sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+sudo apt update
+sudo apt-get install python3.6 -y
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+sudo update-alternatives --config python3
+# lib python
+sudo apt install python3-pip -y
+sudo apt-get install python3-dev libmysqlclient-dev -y
+# curl
+sudo apt-get install curl -y
+# php
+sudo apt-get install libpng-dev -y
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt-get install php-curl -y
+# apache
+sudo apt-get install apache2 -y
+sudo ufw app list
+sudo ufw status
+sudo ufw app info "Apache Full"
+sudo ufw allow in "Apache Full"
+# use .htaccess
+sudo a2enmod rewrite
+# docker
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+# install package python
+sudo pip3 install python-binance
+pip3 install -r requirements.txt
